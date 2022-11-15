a603fee8edbdf56a8ddd22af35c67f0764e0bbb6
Rogerio Gomes <rogerim00@hotmail.com>
2022-11-15 15:28:36 -0300

    Inicio do projeto

---
+++ backend/src/main/java/com/devsuperior/rgmeta/RgmetaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.rgmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/rgmeta/RgmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.rgmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
