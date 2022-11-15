3e16ca1cc866dac23d6700f5dd1a2d8cbde39348
Rogerio Gomes <rogerim00@hotmail.com>
2022-11-15 16:11:56 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/notification-icon.svg'
+
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="rgmeta-red-btn">
+            <img src={icon} alt="img-user" />
+        </div>
+    )
+}
+
+export default NotificationButton
